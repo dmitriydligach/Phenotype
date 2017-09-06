e39c57d0a3d5fa5c5b7ac1e97c570c5e61e58729
dimdym <dimdym@gmail.com>
2017-09-06 12:01:35 -0500

    added a function to load doc labels into a vector

--- Comorbidity/i2b2.py
+++ Comorbidity/i2b2.py
@@ -7,6 +7,7 @@ import os.path
 
 # map labels so this is a binary task
 to_binary = {'Y': 'Yes', 'N': 'No', 'Q': 'No', 'U': 'No'}
+to_int = {'Y': 1, 'N': 0, 'Q': 0, 'U': 0}
 
 def parse_standoff(xml, disease, task):
   """Make patient to class mapping"""
@@ -25,6 +26,33 @@ def parse_standoff(xml, disease, task):
             doc2label[id] = binary_label
 
   return doc2label
+
+def parse_standoff_vectorized(xml, task, exclude=set()):
+  """Map each patient to a vector of labels"""
+
+  # map diseases to integers
+  diseases = get_disease_names(xml, exclude)
+  dis2int = dict([[d, i] for i, d in enumerate(diseases)])
+
+  doc2labels = {} # key: doc id, value: vector of labels
+  tree = et.parse(xml)
+
+  for task_elem in tree.iter('diseases'):
+    if task_elem.attrib['source'] == task:
+      for disease_elem in task_elem:
+
+        for doc_elem in disease_elem:
+          id = doc_elem.attrib['id']
+
+          if not id in doc2labels:
+            doc2labels[id] = [0] * len(dis2int)
+
+          disease_name = disease_elem.attrib['name']
+          disease_label = doc_elem.attrib['judgment']
+          disease_index = dis2int[disease_name]
+          doc2labels[id][disease_index] = to_int[disease_label]
+
+  return doc2labels
 
 def get_disease_names(xml, exclude=set()):
   """Get list of diseases from standoff files"""
@@ -59,8 +87,8 @@ if __name__ == "__main__":
   xml = 'Comorbidity/Xml/obesity_standoff_annotations_test.xml'
   outdir = 'Comorbidity/Text/Test/'
 
-  notes_xml = os.path.join(base, notes)
-  output_dir = os.path.join(base, outdir)
+  annot_xml = os.path.join(base, xml)
 
-  # write_notes_to_files(notes_xml, output_dir)
-  print get_disease_names(os.path.join(base, xml))
+  doc2labels = parse_standoff_vectorized(annot_xml, 'intuitive')
+  print doc2labels
+  
