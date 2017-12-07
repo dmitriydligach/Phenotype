3a580fb650e640056fae479aafb3e3ef6f56330e
dimdym <dimdym@gmail.com>
2017-12-07 13:41:39 -0600

    added the code to map sparse vector to 1000 dim space learned from mimic

--- Comorbidity/svm.py
+++ Comorbidity/svm.py
@@ -108,13 +108,9 @@ def run_evaluation_sparse(disease, judgement, svd=False):
     vocabulary=None,
     binary=False)
   train_count_matrix = vectorizer.fit_transform(x_train)
-  #vectorizer = pickle.load(open('../SVD/cv.p', 'rb'))
-  #train_count_matrix = vectorizer.transform(x_train)
 
-  #tf = TfidfTransformer()
-  #train_tfidf_matrix = tf.fit_transform(train_count_matrix)
-  tf = pickle.load(open('../SVD/tf.p', 'rb'))
-  train_tfidf_matrix = tf.transform(train_count_matrix)
+  tf = TfidfTransformer()
+  train_tfidf_matrix = tf.fit_transform(train_count_matrix)
 
   # now handle the test set
   test_data_provider = DatasetProvider(
@@ -131,14 +127,72 @@ def run_evaluation_sparse(disease, judgement, svd=False):
   test_tfidf_matrix = tf.transform(test_count_matrix)
 
   if svd:
-    # svd = TruncatedSVD(n_components=300)
-    # train_tfidf_matrix = svd.fit_transform(train_tfidf_matrix)
-    # test_tfidf_matrix = svd.transform(test_tfidf_matrix)
+    svd = TruncatedSVD(n_components=300)
+    train_tfidf_matrix = svd.fit_transform(train_tfidf_matrix)
+    test_tfidf_matrix = svd.transform(test_tfidf_matrix)
 
-    svd_model = pickle.load(open('../SVD/svd.p', 'rb'))
-    train_tfidf_matrix = svd_model.transform(train_tfidf_matrix)
-    test_tfidf_matrix = svd_model.transform(test_tfidf_matrix)
+  classifier = LinearSVC(class_weight='balanced', C=1)
+  classifier.fit(train_tfidf_matrix, y_train)
+  predictions = classifier.predict(test_tfidf_matrix)
 
+  p = precision_score(y_test, predictions, average='macro')
+  r = recall_score(y_test, predictions, average='macro')
+  f1 = f1_score(y_test, predictions, average='macro')
+  print 'unique labels in train:', len(set(y_train))
+  print 'p = %.3f' % p
+  print 'r = %.3f' % r
+  print 'f1 = %.3f\n' % f1
+
+  return p, r, f1
+
+def run_evaluation_svd(disease, judgement):
+  """Train on train set and evaluate on test set"""
+
+  print 'disease:', disease
+  print 'judgement:', judgement
+
+  cfg = ConfigParser.ConfigParser()
+  cfg.read(sys.argv[1])
+  base = os.environ['DATA_ROOT']
+  train_data = os.path.join(base, cfg.get('data', 'train_data'))
+  train_annot = os.path.join(base, cfg.get('data', 'train_annot'))
+  test_data = os.path.join(base, cfg.get('data', 'test_data'))
+  test_annot = os.path.join(base, cfg.get('data', 'test_annot'))
+
+  # handle training data first
+  train_data_provider = DatasetProvider(
+    train_data,
+    train_annot,
+    disease,
+    judgement,
+    use_pickled_alphabet=False,
+    alphabet_pickle=cfg.get('data', 'alphabet_pickle'))
+  x_train, y_train = train_data_provider.load_raw()
+  print 'train examples:', len(x_train)
+
+  # load tfidf vectorizer model and transform xs into it
+  vectorizer = pickle.load(open('../Svd/Model/tfidf.p', 'rb'))
+  train_tfidf_matrix = vectorizer.transform(x_train)
+
+  # now handle the test set
+  test_data_provider = DatasetProvider(
+    test_data,
+    test_annot,
+    disease,
+    judgement,
+    use_pickled_alphabet=True,
+    alphabet_pickle=cfg.get('data', 'alphabet_pickle'))
+  x_test, y_test = test_data_provider.load_raw()
+  print 'test examples:', len(x_test)
+  test_tfidf_matrix = vectorizer.transform(x_test)
+
+  # load svd model and map train/test to low dimensions
+  print 'input shape:', train_tfidf_matrix.shape
+  svd = pickle.load(open('../Svd/Model/svd.p', 'rb'))
+  train_tfidf_matrix = svd.transform(train_tfidf_matrix)
+  test_tfidf_matrix = svd.transform(test_tfidf_matrix)
+  print 'output shape:', train_tfidf_matrix.shape
+  
   classifier = LinearSVC(class_weight='balanced', C=1)
   classifier.fit(train_tfidf_matrix, y_train)
   predictions = classifier.predict(test_tfidf_matrix)
@@ -240,8 +294,13 @@ def run_evaluation_all_diseases():
   f1s = []
   for disease in i2b2.get_disease_names(test_annot, exclude):
     if evaluation == 'sparse':
+      # use bag-of-word vectors
       p, r, f1 = run_evaluation_sparse(disease, judgement)
+    elif evaluation == 'svd':
+      # use low dimensional vectors obtained via svd
+      p, r, f1 = run_evaluation_svd(disease, judgement)
     else:
+      # use learned patient vectors
       p, r, f1 = run_evaluation_dense(disease, judgement)
     ps.append(p)
     rs.append(r)
