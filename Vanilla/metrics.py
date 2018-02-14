bf439e0a7122ae6dc122ed2ffd33462a9c976205
dimdym <dimdym@gmail.com>
2018-02-14 10:59:03 -0600

    added roc auc calculation

--- Vanilla/metrics.py
+++ Vanilla/metrics.py
@@ -4,9 +4,12 @@ import sys
 sys.dont_write_bytecode = True
 sys.path.append('../Lib/')
 
-import os
+import os, numpy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
@@ -30,8 +33,27 @@ def load(path):
 
   return examples, labels
 
-def run_experiment():
-  """Split into train and test and run an experiment"""
+def roc():
+  """Get ROC curve"""
+
+  train_examples, train_labels = load(train_path)
+  test_examples, test_labels = load(test_path)
+
+  vectorizer = TfidfVectorizer()
+  train_tfidf_matrix = vectorizer.fit_transform(train_examples)
+  test_tfidf_matrix = vectorizer.transform(test_examples)
+
+  classifier = LogisticRegression(class_weight='balanced')
+  model = classifier.fit(train_tfidf_matrix, train_labels)
+
+  predicted = classifier.predict_proba(test_tfidf_matrix)
+
+  binary_labels = numpy.array(test_labels) == 'Yes'
+  roc_auc = roc_auc_score(binary_labels, predicted[:, 1])
+  print 'roc auc:', roc_auc
+
+def svm():
+  """Train SVM"""
 
   train_examples, train_labels = load(train_path)
   test_examples, test_labels = load(test_path)
@@ -53,4 +75,4 @@ def run_experiment():
 
 if __name__ == "__main__":
 
-  run_experiment()
+  roc()
