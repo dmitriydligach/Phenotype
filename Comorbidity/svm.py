7ff31caedb4848515c27a35ecdd8a7db0cf89743
dimdym <dimdym@gmail.com>
2018-03-22 14:04:55 -0500

    removed old/unused code

--- Comorbidity/svm.py
+++ Comorbidity/svm.py
@@ -27,54 +27,8 @@ import warnings
 warnings.warn = warn
 
 FEATURE_LIST = 'Model/features.txt'
-NUM_FOLDS = 5
 NGRAM_RANGE = (1, 1) # use unigrams for cuis
 MIN_DF = 0
-
-def run_cross_validation_sparse(disease, judgement):
-  """Run n-fold CV on training set"""
-
-  cfg = ConfigParser.ConfigParser()
-  cfg.read(sys.argv[1])
-  base = os.environ['DATA_ROOT']
-  train_data = os.path.join(base, cfg.get('data', 'train_data'))
-  train_annot = os.path.join(base, cfg.get('data', 'train_annot'))
-
-  dataset = DatasetProvider(
-    train_data,
-    train_annot,
-    disease,
-    judgement)
-  x, y = dataset.load_raw()
-
-  # fit builds alphabet; transform extracts counts
-  vectorizer = CountVectorizer(
-    ngram_range=NGRAM_RANGE,
-    stop_words='english',
-    min_df=MIN_DF,
-    vocabulary=None,
-    binary=False)
-  count_matrix = vectorizer.fit_transform(x)
-
-  # print features to file for debugging
-  feature_file = open(FEATURE_LIST, 'w')
-  for feature in vectorizer.get_feature_names():
-    feature_file.write(feature + '\n')
-
-  # transform raw counts to tf-idf
-  tf = TfidfTransformer()
-  tfidf_matrix = tf.fit_transform(count_matrix)
-
-  classifier = LinearSVC(class_weight='balanced', C=10)
-  cv_scores = cross_val_score(
-    classifier,
-    tfidf_matrix,
-    y,
-    scoring='f1_macro',
-    cv=NUM_FOLDS)
-
-  print 'average f1:', numpy.mean(cv_scores)
-  print 'standard devitation:', numpy.std(cv_scores)
 
 def run_evaluation_sparse(disease, judgement, use_svd=False):
   """Train on train set and evaluate on test set"""
