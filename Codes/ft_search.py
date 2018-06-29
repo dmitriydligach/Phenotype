44725c7db567ec25419fcf3b5fed4f4718aef6b2
dimdym <dimdym@gmail.com>
2018-06-29 14:51:31 -0500

    added missing import

--- Codes/ft_search.py
+++ Codes/ft_search.py
@@ -28,7 +28,7 @@ from keras.utils.np_utils import to_categorical
 from keras.optimizers import RMSprop
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
+from keras.layers.core import Dense, Activation, Dropout
 from keras.layers import GlobalAveragePooling1D
 from keras.layers.embeddings import Embedding
 from keras.models import load_model
