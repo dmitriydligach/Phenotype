593898a54a63196714b02d18f716fdcfef740aa6
dimdym <dimdym@gmail.com>
2018-08-01 10:48:30 -0500

    added grid search and removed all n-fold cv function

--- Patient/eval.py
+++ Patient/eval.py
@@ -21,6 +21,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 import keras as k
 from keras.preprocessing.sequence import pad_sequences
@@ -28,22 +29,35 @@ from keras.models import load_model
 from keras.models import Model
 import dataset
 
-def run_eval(x_train, y_train, x_test, y_test):
+def run_eval(x_train, y_train, x_test, y_test, search=True):
   """Evaluation on test set"""
 
-  classifier = LogisticRegression(class_weight='balanced')
-  model = classifier.fit(x_train, y_train)
+  if search:
+    classifier = grid_search(x_train, y_train)
+  else:
+    classifier = LogisticRegression(class_weight='balanced')
+    model = classifier.fit(x_train, y_train)
 
   predictions = classifier.predict(x_test)
   p = precision_score(y_test, predictions, pos_label=1)
   r = recall_score(y_test, predictions, pos_label=1)
   f1 = f1_score(y_test, predictions, pos_label=1)
-  print("\nprecision: %.3f - recall: %.3f - f1: %.3f" % (p, r, f1))
+  print("precision: %.3f - recall: %.3f - f1: %.3f" % (p, r, f1))
 
   probs = classifier.predict_proba(x_test)
   roc_auc = roc_auc_score(y_test, probs[:, 1])
   accuracy = accuracy_score(y_test, predictions)
-  print("\nauc: %.3f - accuracy: %.3f" % (roc_auc, accuracy))
+  print("auc: %.3f - accuracy: %.3f" % (roc_auc, accuracy))
+
+def grid_search(x, y):
+  """Find best model and fit it"""
+
+  param_grid = {'C':[0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+  lr = LogisticRegression(class_weight='balanced')
+  gs = GridSearchCV(lr, param_grid, scoring='roc_auc', cv=10)
+  gs.fit(x, y)
+
+  return gs.best_estimator_
 
 def data_dense():
   """Data to feed into code prediction model"""
@@ -81,9 +95,7 @@ def data_dense():
   x_test = pad_sequences(x_test, maxlen=maxlen)
 
   # make test vectors for target task
-  print('x_test shape (original):', x_test.shape)
   x_test = interm_layer_model.predict(x_test)
-  print('x_test shape (new):', x_test.shape)
 
   return x_train, y_train, x_test, y_test
 
@@ -121,58 +133,6 @@ def data_hybrid():
   x_test = np.concatenate((x_test_dense, x_test_sparse), axis=1)
 
   return x_train, y_train, x_test, y_test
-
-def run_nfold_cv():
-  """N-fold cross validation"""
-
-  cfg = configparser.ConfigParser()
-  cfg.read(sys.argv[1])
-  base = os.environ['DATA_ROOT']
-  data_dir = os.path.join(base, cfg.get('data', 'train'))
-
-  # load target task data
-  dataset_provider = dataset.DatasetProvider(
-    data_dir,
-    cfg.get('data', 'alphabet_pickle'))
-
-  x, y = dataset_provider.load()
-  # pad to same maxlen as data in source model
-  x = pad_sequences(x, maxlen=cfg.getint('data', 'maxlen'))
-  print('x shape (original):', x.shape)
-
-  # make vectors for target task
-  model = load_model(cfg.get('data', 'model_file'))
-  interm_layer_model = Model(inputs=model.input,
-                             outputs=model.get_layer('HL').output)
-  x = interm_layer_model.predict(x)
-  print('x shape (new):', x.shape)
-
-  # ready for svm train/test now
-
-  if cfg.getfloat('data', 'test_size') == 0:
-    # run n-fold cross validation
-    classifier = LinearSVC(class_weight='balanced', C=0.001)
-    cv_scores = cross_val_score(classifier, x, y, scoring='f1', cv=5)
-    print('fold f1s:', cv_scores)
-    print('average f1:', np.mean(cv_scores))
-    print('standard devitation:', np.std(cv_scores))
-
-  else:
-    # randomly allocate a test set
-    x_train, x_test, y_train, y_test = train_test_split(
-      x,
-      y,
-      test_size=cfg.getfloat('data', 'test_size'),
-      random_state=1)
-    classifier = LinearSVC(class_weight='balanced')
-    model = classifier.fit(x_train, y_train)
-    predicted = classifier.predict(x_test)
-    precision = precision_score(y_test, predicted, pos_label=1)
-    recall = recall_score(y_test, predicted, pos_label=1)
-    f1 = f1_score(y_test, predicted, pos_label=1)
-    print('p =', precision)
-    print('r =', recall)
-    print('f1 =', f1)
 
 if __name__ == "__main__":
 
